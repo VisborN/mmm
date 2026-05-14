6a09afa6cb67896a6eacc916f47f2437956b7e74
VisborN <VisborN@users.noreply.github.com>
2026-05-14 19:58:19 +0000

    deploy: 47a8145e1889ab5ae859d2872ca00cf5392344f2

---
+++ sw-ACXKHARP.js
@@ -0,0 +1,50 @@
+// sw.ts
+var sw_default = null;
+var CACHE_NAME = "pwa-cache-v2";
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Service Worker: Caching root index");
+      return cache.addAll(["./"]);
+    })
+  );
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Service Worker: Clearing Old Cache", cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+    // Claim all clients immediately
+  );
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        if (!networkResponse || networkResponse.status !== 200 || event.request.method !== "GET") {
+          return networkResponse;
+        }
+        const responseToCache = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return networkResponse;
+      });
+    })
+  );
+});
+export {
+  sw_default as default
+};
+//# sourceMappingURL=sw-ACXKHARP.js.map
