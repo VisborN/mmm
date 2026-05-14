5ecdb12021c5c1a253f815a9b8348802be1c9942
Dudko Vladislav <dudko.vlad@gmail.com>
2026-05-14 14:20:44 +0000

    fix

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,33 +1,11 @@
-import * as esbuild from 'esbuild';
-import { HtmlEntryPlugin } from '@build-script/esbuild-html-entry';
+#!/usr/bin/env node
+import { configure } from 'esbd';
+import * as path from 'path';
 
-const buildMode = process.argv.includes('--watch');
-
-const buildOptions = {
-  entryPoints: ['web_src/index.html'],
-  bundle: true,
-  minify: !buildMode,
-  sourcemap: true,
-  outdir: 'public',
+configure({
+  entryPoints: ['index.html'],
+  outdir: '../public',
+  absWorkingDir: import.meta.dirname+"/web_src",
+  integrity: "sha256",
   entryNames: '[name]-[hash]',
-  loader: {
-    '.tsx': 'tsx',
-    '.ts': 'ts',
-  },
-    metafile: true, // <-- this is required
-	plugins: [
-		new ESBuildHtmlEntry(), // should be first in most case
-	],
-};
-
-async function run() {
-    if (buildMode) {
-        let ctx = await esbuild.context(buildOptions);
-        await ctx.watch();
-        console.log("Watching...");
-    } else {
-        await esbuild.build(buildOptions);
-    }
-}
-
-run().catch(() => process.exit(1));
+});
