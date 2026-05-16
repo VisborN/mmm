8131068b99e67a1c7dcb9101457e7165e97f27ac
Dudko Vladislav <dudko.vlad@gmail.com>
2026-05-16 19:46:26 +0000

    fix esbd

--- esbuild.mjs
+++ esbuild.mjs
@@ -2,10 +2,17 @@
 import { configure } from 'esbd';
 import * as path from 'path';
 
-configure({
-  entryPoints: ['index.html'],
-  outdir: '../public',
-  absWorkingDir: import.meta.dirname+"/web_src",
-  integrity: "sha256",
-  entryNames: '[name]-[hash]',
-});
+configure([
+  {
+    entryPoints: ['sw.ts'],
+    outdir: '../public',
+    absWorkingDir: import.meta.dirname + "/web_src",
+  },
+  {
+    entryPoints: ['index.html'],
+    outdir: '../public',
+    absWorkingDir: import.meta.dirname + "/web_src",
+    integrity: "sha256",
+    entryNames: '[name]-[hash]',
+  }
+]);
--- web_src/app.tsx
+++ web_src/app.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { AppMain } from './app_component';
 
 document.addEventListener('DOMContentLoaded', () => {
+
     const el = document.getElementById('app');
     if (!el) {
         throw new Error(`Container with id 'app' not found.`);
@@ -10,3 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const root = createRoot(el);
     root.render(<AppMain />);
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.ts.js' )
+            .then(reg => console.log('SW registered:', reg))
+            .catch(err => console.error('SW registration failed:', err));
+    });
+}
