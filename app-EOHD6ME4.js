0d4a948301d1de1ea800c4d0026b02db59a50f5f
VisborN <VisborN@users.noreply.github.com>
2026-05-19 17:00:32 +0000

    deploy: 5c2a5a0e2a0f51fd32a9cd609f27ce0414966462

--- app-EOHD6ME4.js
+++ app-EOHD6ME4.js
@@ -28109,7 +28109,13 @@ var AppMain = observer(() => {
       backgroundColor: "white",
       borderBottom: "1px solid #eee"
     }, children: [
-      /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("h1", { style: { margin: 0, fontSize: "24px", fontWeight: "normal" }, children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
+      /* @__PURE__ */ (0, import_jsx_runtime3.jsxs)("div", { children: [
+        /* @__PURE__ */ (0, import_jsx_runtime3.jsx)("h1", { style: { margin: 0, fontSize: "24px", fontWeight: "normal" }, children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
+        /* @__PURE__ */ (0, import_jsx_runtime3.jsxs)("div", { style: { fontSize: "10px", color: "#888", marginTop: "2px" }, children: [
+          "v. ",
+          true ? "2026-05-19 19:00:03 +0200" : "dev"
+        ] })
+      ] }),
       /* @__PURE__ */ (0, import_jsx_runtime3.jsxs)("div", { style: { display: "flex", gap: "16px" }, children: [
         /* @__PURE__ */ (0, import_jsx_runtime3.jsx)(
           "button",
@@ -28285,4 +28291,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-BA2RS5VI.js.map
+//# sourceMappingURL=app-EOHD6ME4.js.map
