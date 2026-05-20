bd1b8ac7852e94168c6f8826034a85ebe5bfe4e4
VisborN <VisborN@users.noreply.github.com>
2026-05-20 18:20:58 +0000

    deploy: 58097e324c025f822845509faf665504319a9e2c

--- app-JF6YV5F3.js
+++ app-JF6YV5F3.js
@@ -27376,7 +27376,7 @@ var import_globals4 = __toESM(require_globals());
 // infrastructure/google_drive.ts
 var import_globals3 = __toESM(require_globals());
 var CLIENT_ID = "26384778878-6ev9883mob866lnecidhqeudl4461pvd.apps.googleusercontent.com";
-var SCOPES = "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets";
+var SCOPES = "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets";
 var GoogleDriveService = class {
   constructor() {
     __publicField(this, "accessToken", null);
@@ -28327,7 +28327,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime4.jsx)("h1", { style: { margin: 0, fontSize: "24px", fontWeight: "normal" }, children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { fontSize: "10px", color: "#888", marginTop: "2px" }, children: [
           "v. ",
-          true ? "2026-05-19 23:57:09 +0200" : "dev"
+          true ? "2026-05-20 20:20:16 +0200" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { display: "flex", gap: "16px" }, children: [
@@ -28523,4 +28523,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-MZ35EFVQ.js.map
+//# sourceMappingURL=app-JF6YV5F3.js.map
