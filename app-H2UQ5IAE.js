1d77f91c6f99fe22fb00fc17f58136c22a27daf8
VisborN <VisborN@users.noreply.github.com>
2026-05-21 20:09:35 +0000

    deploy: 825bf7b0f6704bea9c5b18f3072615e104e38831

--- app-H2UQ5IAE.js
+++ app-H2UQ5IAE.js
@@ -28379,26 +28379,31 @@ var AccountsView = observer(() => {
 
 // db_explorer_view.tsx
 var import_react8 = __toESM(require_react());
+var import_globals5 = __toESM(require_globals());
 var import_jsx_runtime2 = __toESM(require_jsx_runtime());
 var DatabaseExplorer = observer(() => {
   const [dbData, setDbData] = (0, import_react8.useState)({});
   const [loading, setLoading] = (0, import_react8.useState)(true);
   (0, import_react8.useEffect)(() => {
     const loadData = async () => {
-      try {
-        const db = await getDB();
-        const storeNames = db.objectStoreNames;
-        const allData = {};
-        for (let i = 0; i < storeNames.length; i++) {
-          const storeName = storeNames[i];
-          allData[storeName] = await db.getAll(storeName);
-        }
-        setDbData(allData);
-      } catch (err3) {
-        console.error("Failed to load DB data", err3);
-      } finally {
+      const dbRes = await withResult(getDB)();
+      if (dbRes.error !== null) {
+        console.error("Failed to load database:", dbRes.error);
         setLoading(false);
+        return;
       }
+      const db = dbRes.data;
+      const storeNames = db.objectStoreNames;
+      const allData = {};
+      for (let i = 0; i < storeNames.length; i++) {
+        const storeName = storeNames[i];
+        const res = await withResult(db.getAll, db)(storeName);
+        if (!res.error) {
+          allData[storeName] = res.data;
+        }
+      }
+      setDbData(allData);
+      setLoading(false);
     };
     loadData();
   }, []);
@@ -28746,7 +28751,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime4.jsx)("h1", { style: { margin: 0, fontSize: "24px", fontWeight: "normal" }, children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { fontSize: "10px", color: "#888", marginTop: "2px" }, children: [
           "v. ",
-          true ? "2026-05-21 19:11:40 +0200" : "dev"
+          true ? "2026-05-21 22:09:11 +0200" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { display: "flex", gap: "16px" }, children: [
@@ -28951,4 +28956,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-5NOTWIQA.js.map
+//# sourceMappingURL=app-H2UQ5IAE.js.map
