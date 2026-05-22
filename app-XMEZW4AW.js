a1f728552543d4103a4e52ecb0980c3b0a802088
VisborN <VisborN@users.noreply.github.com>
2026-05-22 00:05:40 +0000

    deploy: 589d369a0ff28a704ab01edc61e15b668bdac9e6

--- app-XMEZW4AW.js
+++ app-XMEZW4AW.js
@@ -28827,7 +28827,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime4.jsx)("h1", { style: { margin: 0, fontSize: "24px", fontWeight: "normal" }, children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { fontSize: "10px", color: "#888", marginTop: "2px" }, children: [
           "v. ",
-          true ? "2026-05-22 01:56:46 +0200" : "dev"
+          true ? "2026-05-22 02:05:19 +0200" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { display: "flex", gap: "16px" }, children: [
@@ -29046,4 +29046,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-TJNLCI4Y.js.map
+//# sourceMappingURL=app-XMEZW4AW.js.map
