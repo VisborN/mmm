9e098f8699136370d13c3766458fbb23a097fc08
VisborN <VisborN@users.noreply.github.com>
2026-05-22 19:35:55 +0000

    deploy: 910d45789f9859bd0a0af373d091da90b0242f88

--- app-YBCIONII.js
+++ app-YBCIONII.js
@@ -28877,7 +28877,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime4.jsx)("h1", { style: { margin: 0, fontSize: "24px", fontWeight: "normal" }, children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { fontSize: "10px", color: "#888", marginTop: "2px" }, children: [
           "v. ",
-          true ? "2026-05-22 21:20:18 +0200" : "dev"
+          true ? "2026-05-22 21:35:33 +0200" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { display: "flex", gap: "16px" }, children: [
@@ -29096,4 +29096,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-PLZSCEAZ.js.map
+//# sourceMappingURL=app-YBCIONII.js.map
