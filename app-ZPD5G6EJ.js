005a1be001b9313dd264fb328af8faf1ae24c91d
VisborN <VisborN@users.noreply.github.com>
2026-07-09 19:13:59 +0000

    deploy: 49e7fb479a656b19082955324bc268993c641a95

--- app-ZPD5G6EJ.js
+++ app-ZPD5G6EJ.js
@@ -29047,7 +29047,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime4.jsx)("h1", { style: { margin: 0, fontSize: "24px", fontWeight: "normal" }, children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { fontSize: "10px", color: "#888", marginTop: "2px" }, children: [
           "v. ",
-          true ? "2026-07-09 18:45:35 +0300" : "dev"
+          true ? "2026-07-09 22:13:35 +0300" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { display: "flex", gap: "16px" }, children: [
@@ -29258,4 +29258,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-4M7AI3JS.js.map
+//# sourceMappingURL=app-ZPD5G6EJ.js.map
