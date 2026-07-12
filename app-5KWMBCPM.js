8ddb3035aee5ab0488eba354c990f88013f0b825
VisborN <VisborN@users.noreply.github.com>
2026-07-12 21:38:30 +0000

    deploy: 5a7ac3a10670bbbac10de6c9a9d2aac1d73406fa

--- app-5KWMBCPM.js
+++ app-5KWMBCPM.js
@@ -29149,7 +29149,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime4.jsx)("h1", { style: { margin: 0, fontSize: "24px", fontWeight: "normal" }, children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { fontSize: "10px", color: "#888", marginTop: "2px" }, children: [
           "v. ",
-          true ? "2026-07-13 00:32:03 +0300" : "dev"
+          true ? "2026-07-13 00:38:10 +0300" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { display: "flex", gap: "16px" }, children: [
@@ -29360,4 +29360,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-PTSTQSS4.js.map
+//# sourceMappingURL=app-5KWMBCPM.js.map
