1f532710455a744f6d2bfa0201c3df524ba6ea94
VisborN <VisborN@users.noreply.github.com>
2026-07-12 16:00:17 +0000

    deploy: bd4c60c33fac251ebc83121991c1c42225a536db

--- app-SSJ6ZIR7.js
+++ app-SSJ6ZIR7.js
@@ -29160,7 +29160,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime4.jsx)("h1", { style: { margin: 0, fontSize: "24px", fontWeight: "normal" }, children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { fontSize: "10px", color: "#888", marginTop: "2px" }, children: [
           "v. ",
-          true ? "2026-07-12 18:55:12 +0300" : "dev"
+          true ? "2026-07-12 18:59:51 +0300" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime4.jsxs)("div", { style: { display: "flex", gap: "16px" }, children: [
@@ -29371,4 +29371,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-YAEKYOTY.js.map
+//# sourceMappingURL=app-SSJ6ZIR7.js.map
