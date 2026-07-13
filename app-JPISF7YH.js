1653478c9a541d882c6b0a1eb55058d46c2830ef
VisborN <VisborN@users.noreply.github.com>
2026-07-13 05:50:48 +0000

    deploy: a1034f64df7c37ca7b693ecebbb367087fb7a728

--- app-JPISF7YH.js
+++ app-JPISF7YH.js
@@ -29593,7 +29593,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime8.jsx)("h1", { className: "app-title", children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime8.jsxs)("div", { className: "app-version", children: [
           "v. ",
-          true ? "2026-07-13 08:38:19 +0300" : "dev"
+          true ? "2026-07-13 08:50:22 +0300" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime8.jsxs)("div", { className: "header-actions", children: [
@@ -29744,4 +29744,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-E3ZB6UCS.js.map
+//# sourceMappingURL=app-JPISF7YH.js.map
