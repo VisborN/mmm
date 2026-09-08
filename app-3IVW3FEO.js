ffd6fdbe69cab2188304a8b7488ff7abe93c7b05
VisborN <VisborN@users.noreply.github.com>
2026-09-08 09:07:28 +0000

    deploy: d780f0c2f25129cf87ebf42ba8120102a38fdefc

--- app-3IVW3FEO.js
+++ app-3IVW3FEO.js
@@ -29952,7 +29952,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime8.jsx)("h1", { className: "app-title", children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime8.jsxs)("div", { className: "app-version", children: [
           "v. ",
-          true ? "2026-09-08 11:59:03 +0300" : "dev"
+          true ? "2026-09-08 12:06:58 +0300" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime8.jsxs)("div", { className: "header-actions", children: [
@@ -30103,4 +30103,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-BR46W7WQ.js.map
+//# sourceMappingURL=app-3IVW3FEO.js.map
--- sw.ts.js
+++ sw.ts.js
@@ -2,7 +2,7 @@
 (() => {
   // sw.ts
   var sw_default = null;
-  var CACHE_NAME = `mmm-pwa-${"2026-09-08 11:59:03 +0300" ? "2026-09-08 11:59:03 +0300".replace(/\s+/g, "-") : "v1"}`;
+  var CACHE_NAME = `mmm-pwa-${"2026-09-08 12:06:58 +0300" ? "2026-09-08 12:06:58 +0300".replace(/\s+/g, "-") : "v1"}`;
   var PRECACHE_ASSETS = [
     "./",
     "./index.html",
