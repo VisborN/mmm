cf2cad8c5d02cb6de80e68f0fd4e529c187b5d96
VisborN <VisborN@users.noreply.github.com>
2026-09-08 20:18:56 +0000

    deploy: eb692e7afe81d92c2395a4f782113f06511165e4

--- sw.ts.js
+++ sw.ts.js
@@ -2,7 +2,7 @@
 (() => {
   // sw.ts
   var sw_default = null;
-  var CACHE_NAME = `mmm-pwa-${"2026-09-08 22:14:30 +0300" ? "2026-09-08 22:14:30 +0300".replace(/\s+/g, "-") : "v1"}`;
+  var CACHE_NAME = `mmm-pwa-${"2026-09-08 23:18:30 +0300" ? "2026-09-08 23:18:30 +0300".replace(/\s+/g, "-") : "v1"}`;
   var PRECACHE_ASSETS = [
     "./",
     "./index.html",
