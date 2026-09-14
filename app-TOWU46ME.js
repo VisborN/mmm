3cbd8bf7f9ea96d62b618a5fa8426fe355a898bd
VisborN <VisborN@users.noreply.github.com>
2026-09-14 08:01:20 +0000

    deploy: d488db2b60a2b6450dcc69856d4bf41d8a4906fc

--- app-TOWU46ME.js
+++ app-TOWU46ME.js
@@ -32564,7 +32564,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime9.jsx)("h1", { className: "app-title", children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime9.jsxs)("div", { className: "app-version", children: [
           "v. ",
-          true ? "2026-09-14 11:00:22 +0300" : "dev"
+          true ? "2026-09-14 11:00:52 +0300" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime9.jsxs)("div", { className: "header-actions", children: [
@@ -32716,4 +32716,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-JP7U7CN6.js.map
+//# sourceMappingURL=app-TOWU46ME.js.map
