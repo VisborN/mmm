5a57dab518161bd16d35b70cb18ea81c2be0d500
VisborN <VisborN@users.noreply.github.com>
2026-09-18 17:49:46 +0000

    deploy: 31d15d5057f3516e3eede2d68f8b0c5ca9db15e2

--- app-5FJEXDND.js
+++ app-5FJEXDND.js
@@ -31364,7 +31364,7 @@ var AppMain = observer(() => {
         /* @__PURE__ */ (0, import_jsx_runtime8.jsx)("h1", { className: "app-title", children: "\u043C\u043E\u043D\u0435\u0439 \u0444\u043B\u043E\u0432" }),
         /* @__PURE__ */ (0, import_jsx_runtime8.jsxs)("div", { className: "app-version", children: [
           "v. ",
-          true ? "2026-09-18 20:04:05 +0300" : "dev"
+          true ? "2026-09-18 20:49:20 +0300" : "dev"
         ] })
       ] }),
       /* @__PURE__ */ (0, import_jsx_runtime8.jsxs)("div", { className: "header-actions", children: [
@@ -31515,4 +31515,4 @@ react/cjs/react-jsx-runtime.development.js:
    * LICENSE file in the root directory of this source tree.
    *)
 */
-//# sourceMappingURL=app-5LJXZICQ.js.map
+//# sourceMappingURL=app-5FJEXDND.js.map
